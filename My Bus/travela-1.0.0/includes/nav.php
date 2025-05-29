274a12afbc9ec018f078bafabb7bfca219ea1b02
kirtipatel45 <patelkirti1445@gmail.com>
2025-05-29 19:24:30 +0530

    Add files via upload

---
+++ My Bus/travela-1.0.0/includes/nav.php
@@ -0,0 +1,26 @@
+ <div class="container-fluid position-relative p-0">
+     <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+         <a href="index.php" class="navbar-brand p-0">
+             <h1 class="m-0"><i class="fa fa-map-marker-alt me-3"></i>My Bus</h1>
+             <!-- <img src="img/logo.png" alt="Logo"> -->
+         </a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+             <span class="fa fa-bars"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarCollapse">
+             <div class="navbar-nav ms-auto py-0">
+                 <a href="./index.php" class="nav-item nav-link active">Home</a>
+                 <a href="./about.php" class="nav-item nav-link">About</a>
+                 <a href="./contact.php" class="nav-item nav-link">Contact</a>
+             </div>
+             <a href="index.php" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Book Now</a>
+         </div>
+         <div class="dropdown">
+            <a href="#" class="dropdown-toggle text-light" style="margin-left: 18px" data-bs-toggle="dropdown"><small><i class="fa fa-home me-2"></i> My Dashboard</small></a>
+            <div class="dropdown-menu rounded">
+                <a href="mybooking.php" class="dropdown-item"><i class="fas fa-user-alt me-2"></i> My Booking</a>
+                <a href="logout.php" class="dropdown-item"><i class="fas fa-user-alt me-2"></i> Logout</a>
+            </div>
+           
+        </div>
+     </nav>
---
+++ My Bus/travela-1.0.0/includes/slider.php
@@ -0,0 +1,76 @@
+  <div class="carousel-header">
+      <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+          <ol class="carousel-indicators">
+              <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active"></li>
+              <li data-bs-target="#carouselId" data-bs-slide-to="1"></li>
+              <li data-bs-target="#carouselId" data-bs-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                  <img src="images/2.png" class="img-fluid" alt="Image" />
+                  <div class="carousel-caption">
+                      <div class="p-3" style="max-width: 900px">
+                          <h4 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px">
+                              Explore The World
+                          </h4>
+                          <h1 class="display-2 text-capitalize text-white mb-4">
+                          Exceptional Customer Service
+                          </h1>
+                          <p class="mb-5 fs-5">
+                          Our dedicated customer service team is committed to providing personalized assistance and support to every customer. Whether you have questions about your booking, need help with itinerary changes, or require assistance during your journey, we're here to help.
+                          </p>
+                          <div class="d-flex align-items-center justify-content-center">
+                              <a class="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <img src="images/3.png" class="img-fluid" alt="Image" />
+                  <div class="carousel-caption">
+                      <div class="p-3" style="max-width: 900px">
+                          <h4 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px">
+                          User-Friendly Interface
+                          </h4>
+                          <h1 class="display-2 text-capitalize text-white mb-4">
+                              Find Your Perfect Tour At Travel
+                          </h1>
+                          <p class="mb-5 fs-5">
+                          Our intuitive website and mobile app make it easy for travelers to search, compare, and book bus tickets with ease. With a few simple clicks, passengers can find the best routes, select their preferred seats, and complete their bookings in no time.
+                          </p>
+                          <div class="d-flex align-items-center justify-content-center">
+                              <a class="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="carousel-item">
+                  <img src="images/4.png" class="img-fluid" alt="Image" />
+                  <div class="carousel-caption">
+                      <div class="p-3" style="max-width: 900px">
+                          <h4 class="text-white text-uppercase fw-bold mb-4" style="letter-spacing: 3px">
+                              Explore The World
+                          </h4>
+                          <h1 class="display-2 text-capitalize text-white mb-4">
+                          Extensive Network
+                          </h1>
+                          <p class="mb-5 fs-5">
+                          With an extensive network of bus operators and routes, we offer travelers access to a wide range of destinations across the region. Whether you're traveling for business or leisure, we've got you covered with convenient options to suit your needs.
+                          </p>
+                          <div class="d-flex align-items-center justify-content-center">
+                              <a class="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon btn bg-primary" aria-hidden="false"></span>
+              <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+              <span class="carousel-control-next-icon btn bg-primary" aria-hidden="false"></span>
+              <span class="visually-hidden">Next</span>
+          </button>
+      </div>
+  </div>
